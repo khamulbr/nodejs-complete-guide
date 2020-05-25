0227f3099a5d67894ae89eb4110daadd48fc981b
Alessandro <alessandro.valerio.dias@gmail.com>
2020-05-25 17:54:03 -0300

    after some classes yet

--- play.js
+++ play.js
@@ -34,18 +34,26 @@ const person = {
     }
 };
 
-const printName = (personData) => {
-    console.log(personData.name);
+const printName = ({ name }) => {
+    console.log(name);
 }
 
 printName(person);
+
+const { name, age } = person;
+
+console.log(name, age);
 
 // person.greet();
 
 // const copiedPerson = {...person};
 // console.log(copiedPerson);
 
-// const hobbies = ['Sports', 'Cooking'];
+const hobbies = ['Sports', 'Cooking'];
+
+const [hobby1, hobby2] = hobbies;
+
+console.log(hobby1, hobby2);
 
 // for (let hobby of hobbies){
 //     console.log(hobby);
