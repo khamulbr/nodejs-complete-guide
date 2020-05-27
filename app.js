ced1a8c6f796d638c680d2406d864fd2dca5250c
Alessandro <alessandro.valerio.dias@gmail.com>
2020-05-27 00:51:29 -0300

    server now getting param from form

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    //process.exit();
+    const url = req.url;
+    const method = req.method;
+
     res.setHeader('Content-Type', 'text/html');
+
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    } 
+
+    if (url === '/message' && method === 'POST' ) {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+
     res.write('<html>');
     res.write('<head><title>My first page</title></head>');
     res.write('<body><h1>Hello from my Node.js Server</h1></body>');
